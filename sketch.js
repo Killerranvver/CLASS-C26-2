54907b8a3c39055e8e9ce63536a5f6e4c6b6d3de
Killerranveer <ranveerlalwani1109@gmail.com>
2021-05-31 17:12:46 +0800

    Hello

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function setup(){
     world = engine.world;
 
 
-    ground = new Ground(600,height,1201,20);
+    ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 298, 168);
 
     box1 = new Box(700,320,70,70);
